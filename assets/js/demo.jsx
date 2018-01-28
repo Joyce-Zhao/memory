8a3ea82739b461bd264b8c75b72b0512c5baaa77
joyce1125 <zhao.xue1@husky.neu.edu>
2018-01-28 15:29:49 -0500

    first try

--- assets/js/demo.jsx
+++ assets/js/demo.jsx
@@ -3,48 +3,153 @@ import ReactDOM from 'react-dom';
 import { Button } from 'reactstrap';
 
 export default function run_demo(root) {
-  ReactDOM.render(<Demo side={0}/>, root);
+  ReactDOM.render(<Demo />, root);
 }
+
+// App state for Memory is:
+// {
+//    clicknum: Number     // the number of clicks
+//    guess:    String     // the letter associated with the first tile clicked
+//    tiles:  [List of TileItem]
+// }
+//
+// A TileItem is:
+//   { name: String, count: Number, clicked: Bool, done: Bool }
 
 class Demo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { side: props.side };
+    this.state = {
+      clicknum: 0,
+      guess: "",
+      tiles:  [
+        { name: "A", count: 1, clicked: false, done: false }
+        { name: "B", count: 2, clicked: false, done: false }
+        { name: "C", count: 3, clicked: false, done: false }
+        { name: "D", count: 4, clicked: false, done: false }
+        { name: "E", count: 5, clicked: false, done: false }
+        { name: "F", count: 6, clicked: false, done: false }
+        { name: "G", count: 7, clicked: false, done: false }
+        { name: "H", count: 8, clicked: false, done: false }
+        { name: "A", count: 9, clicked: false, done: false }
+        { name: "B", count: 10, clicked: false, done: false }
+        { name: "C", count: 11, clicked: false, done: false }
+        { name: "D", count: 12, clicked: false, done: false }
+        { name: "E", count: 13, clicked: false, done: false }
+        { name: "F", count: 14, clicked: false, done: false }
+        { name: "G", count: 15, clicked: false, done: false }
+        { name: "H", count: 16, clicked: false, done: false }
+      ],
+    };
   }
 
-  toggle(side) {
-    var side = +!this.state.side;
-    this.setState({side: side});
+  markItem(name) {
+    let xs = _.map(this.state.items, (item) => {
+      clicknum += 1;
+      item.style.display = "block";
+      if (item.name == name) {
+        if (guess != "") {
+          let cur = guess;
+          guess = "";
+          if (item.name == cur) {
+            return _.extend(document.getElementsByName(item.name), {clicked: true, done: true});
+          }
+          else {
+            document.getElementsByName(cur).style.display = "none";
+            document.getElementsByName(item.name).style.display = "none";
+            return _.extend(document.getElementsByName(cur), {clicked: false});
+          }
+        }
+        else {
+          guess = item.name;
+          return _.extend(item, {clicked: true});
+        }
+      }
+      else {
+        return item;
+      }
+    });
+    this.setState({ items: xs });
+  }
+
+  replay() {
+    this.setState({
+      clicknum: 0,
+      guess: "",
+      tiles:  [
+        { name: "A", count: 1, clicked: false, done: false }
+        { name: "B", count: 2, clicked: false, done: false }
+        { name: "C", count: 3, clicked: false, done: false }
+        { name: "D", count: 4, clicked: false, done: false }
+        { name: "E", count: 5, clicked: false, done: false }
+        { name: "F", count: 6, clicked: false, done: false }
+        { name: "G", count: 7, clicked: false, done: false }
+        { name: "H", count: 8, clicked: false, done: false }
+        { name: "A", count: 9, clicked: false, done: false }
+        { name: "B", count: 10, clicked: false, done: false }
+        { name: "C", count: 11, clicked: false, done: false }
+        { name: "D", count: 12, clicked: false, done: false }
+        { name: "E", count: 13, clicked: false, done: false }
+        { name: "F", count: 14, clicked: false, done: false }
+        { name: "G", count: 15, clicked: false, done: false }
+        { name: "H", count: 16, clicked: false, done: false }
+      ],
+    }
+  );
   }
 
   render() {
-    var toggle = this.toggle.bind(this);
+    let tile_list = _.map(this.state.tiles, (item) => {
+      return <TileItem item={item} markItem={this.markItem.bind(this)} />;
+    });
     return (
+      <div>
       <div className="row">
-        <Side show={this.state.side == 0} toggle={toggle} />
-        <div className="col">
-          &nbsp;
+        {tile_list}
+        /*
+        <div className="col">A</div>
+        <div className="col">B</div>
+        <div className="col">C</div>
+        <div className="col">C</div>
+        <div className="w-100"></div>
+        <div className="col">D</div>
+        <div className="col">E</div>
+        <div className="col">F</div>
+        <div className="col">F</div>
+        <div className="w-100"></div>
+        <div className="col">G</div>
+        <div className="col">H</div>
+        <div className="col">A</div>
+        <div className="col">B</div>
+        <div className="w-100"></div>
+        <div className="col">D</div>
+        <div className="col">E</div>
+        <div className="col">G</div>
+        <div className="col">H</div>
+        */
+      </div>
+      <div>
+          <h2>
+            Current Number of Clicks : {this.state.clicknum}
+          </h2>
+          <h2>
+            Current Score : {Math.round(30 - this.state.clicknum)}
+          </h2>
         </div>
-        <Side show={this.state.side == 1} toggle={toggle} />
+        <button type="button" className={"btn btn-primary"} onClick={this.replay}>
+          {"Replay"}
+        </button>
       </div>
     );
   }
 }
 
-function Side(params) {
-  if (params.show) {
-    return (
-      <div id="side-0" className="side col" onMouseOver={ () => params.toggle() }>
-        <Button onClick={ () => alert("cheater") }>Click Me</Button>
-      </div>
-    );
+function TileItem(props) {
+  let item = props.item;
+  if (item.done) {
+    return <div className="col-lg">Done</div>;
   }
   else {
-    return (
-      <div id="side-0" className="side col">
-        &nbsp;
-      </div>
-    );
+    return <div className="col-lg" display: none onClick={() => props.markItem(item.name)}>item.name</div>;
   }
 }
-
